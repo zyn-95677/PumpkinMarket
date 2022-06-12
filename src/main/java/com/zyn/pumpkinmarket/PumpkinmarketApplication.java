ab89b3f35e444b154c38705a36fd206bb76b0463
zyn-95677 <565964094@qq.com>
2022-06-12 11:11:09 +0800

    init

---
+++ src/main/java/com/zyn/pumpkinmarket/PumpkinmarketApplication.java
@@ -0,0 +1,13 @@
+package com.zyn.pumpkinmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpkinmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PumpkinmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyn/pumpkinmarket/PumpkinmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyn.pumpkinmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpkinmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
